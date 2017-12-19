9085480a2240fee1626087224718cf02c803c8ef
William Koch <wkochbrg@gmail.com>
2017-12-19 04:34:57 -0200

    corrected version

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Haiku
     class Program
     {
         public static readonly string AppName = "Haiku";
-        public static readonly string AppVersion = "0.8.0";
+        public static readonly string AppVersion = "0.9.0";
 
 
         static void Main(string[] args)
